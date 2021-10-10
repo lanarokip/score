df0a07809be599755f955cf9175c64bc41821f34
lanarokip <lankipkoecharo@gmail.com>
2021-10-10 11:03:12 +0300

    added the array holding the countries the app gonna be displaying.also added the butterknife to prevent redundsancy to the code

--- app/src/main/java/com/kipkoech/score/livescore.java
+++ app/src/main/java/com/kipkoech/score/livescore.java
@@ -2,13 +2,29 @@ package com.kipkoech.score;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class livescore extends AppCompatActivity {
+    @BindView(R.id.listView) ListView mListview;
+    private String[] countries = new String[] {"Kenya","Tanzania","Uganda","Egypt","Nigeria"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_livescore);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,countries);
+        mListview.setAdapter(adapter);
+
+        Intent intent = getIntent();
+
+
     }
 }
