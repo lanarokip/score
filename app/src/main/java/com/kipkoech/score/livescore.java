d385847debbb579a0fcb932ba30413c934a056cd
lanarokip <lankipkoecharo@gmail.com>
2021-10-10 11:25:57 +0300

    added a toast to the counties list to atest if they work as intended that is whe a country is clicked it should display all games in that country

--- app/src/main/java/com/kipkoech/score/livescore.java
+++ app/src/main/java/com/kipkoech/score/livescore.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,8 +27,13 @@ public class livescore extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,countries);
         mListview.setAdapter(adapter);
 
-        Intent intent = getIntent();
-
+        mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String countries = ((TextView)view).getText().toString();
+                Toast.makeText(livescore.this,countries,Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
